c6f2bbe30b681560695c0bd35231a5c9bdd9e141
AndrzejBru <71904136+AndrzejBru@users.noreply.github.com>
2022-12-07 22:44:20 +0100

    zmiana

--- body.py
+++ body.py
@@ -1,10 +1,11 @@
 from tkinter import *
+import tkinter.messagebox
 #from PIL import ImageTK, Image
 import sqlite3
 
 root = Tk()
 root.title('Brudownik')
-root.geometry('400x800')
+root.geometry('800x800')
 
 # Database
 
@@ -24,6 +25,26 @@ c.execute("""CREATE TABLE IF NOT EXISTS addresses (
         data_oddania integer,
         data_zwrotu integer
         )""")
+
+# Create Frames 
+
+MainFrame = Frame(root, bg ="cadet blue")
+MainFrame.grid()
+
+TitFrame = Frame (MainFrame, bd=2, padx=54, pady=8, bg="Ghost White", relief=RIDGE )
+TitFrame.pack(side=TOP)
+
+lblTit = Label(TitFrame, font=('arial', 40, 'bold'), text="Program do ubrań", bg="Ghost White")
+lblTit.grid()
+
+Data = Frame(MainFrame, bd=1, width=700, height=400, padx=20, pady=20, relief=RIDGE, bg="cadet blue")
+Data.pack(side=BOTTOM)
+
+DataFrame = Frame(Data, bd=1, width=300, height=400, padx=20, pady=20, relief=RIDGE, bg="cadet blue")
+DataFrame.pack(side=LEFT)
+
+DataView = Frame(Data,  bd=1, width=400, height=400, padx=20, pady=20, relief=RIDGE, bg="cadet blue")
+DataFrame.pack(side=RIGHT)
 
 # Create Submit Function For database
 def submit():
@@ -70,65 +91,79 @@ def View():
         c.execute("SELECT * FROM addresses")
 
         records = c.fetchall()
-        
+        print(records)
         print_records = ''
         for record in records:
                 print_records += str(record) + str(record[5]) +"\n"
 
-        view_label = Label(root, text=print_records)
-        view_label.grid(row=8, column=0, columnspan=2)
+        view_label = Label(DataView, text=print_records)
+        view_label.grid(row=8, columnspan=2, column=0)
         conn.commit()
 
         conn.close()
 
 # Możliwe błędne działanie wyświetlania tabeli DO SPRAWDZENIA
 
-numer = Entry(root, width=30)
+numer = Entry(DataFrame, width=30)
 numer.grid(row=0, column=1, padx=20)
 
-ubranie = Entry(root, width=30)
+ubranie = Entry(DataFrame, width=30)
 ubranie.grid(row=1, column=1)
 
-numer_ubr = Entry(root, width=30)
+numer_ubr = Entry(DataFrame, width=30)
 numer_ubr.grid(row=2, column=1)
 
-data_w = Entry(root, width=30)
+data_w = Entry(DataFrame, width=30)
 data_w.grid(row=3, column=1)
 
-data_od = Entry(root, width=30)
+data_od = Entry(DataFrame, width=30)
 data_od.grid(row=4, column=1)
 
-data_zwr = Entry(root, width=30)
+data_zwr = Entry(DataFrame, width=30)
 data_zwr.grid(row=5, column=1)
 
 #Create Text Box Lablels
 
-numer_label = Label(root, text='id')
-numer_label.grid(row=0, column=0,)
+numer_label = Label(DataFrame, text='id')
+numer_label.grid(row=0, column=0)
 
-ubranie_label = Label(root, text='Typ ubrania')
+ubranie_label = Label(DataFrame, text='Typ ubrania')
 ubranie_label.grid(row=1, column=0)
 
-numer_ubr_label = Label(root, text='Numer ubrania')
+numer_ubr_label = Label(DataFrame, text='Numer ubrania')
 numer_ubr_label.grid(row=2, column=0)
 
-data_w_label = Label(root, text='Data wydania ubrania')
+data_w_label = Label(DataFrame, text='Data wydania ubrania')
 data_w_label.grid(row=3, column=0)
 
-data_od_label = Label(root, text='Data oddania do prania')
+data_od_label = Label(DataFrame, text='Data oddania do prania')
 data_od_label.grid(row=4, column=0)
 
-data_zwr_label = Label(root, text='Data zwrotu z pralni')
+data_zwr_label = Label(DataFrame, text='Data zwrotu z pralni')
 data_zwr_label.grid(row=5, column=0)
+
+# Create ListBox $ Scrollbar Widget
+
+#DataFrameRIGHT = Frame(DataFrame, bd=1, width=450, height=300, padx=31, pady=3, relief=3, bg='Ghost Wite')
+#DataFrameRIGHT.grid(row=0, column=3)
+
+#scrollbar = Scrollbar(DataFrameRIGHT)
+#scrollbar.grid(row=0, column=3, sticky='ns')
+
+#studentlist = Listbox(DataFrameRIGHT, width=41, height=16, font=('arial', 12, 'bold'), yscrollcommand=scrollbar.set)
+studentlist = Listbox(DataView, width=41, height=20, font=('arial', 12, 'bold'))
+studentlist.grid(row=0, column=4)
+#scrollbar.config(command= studentlist.yview)
+
 
 # Create Submit Butto
 
-submit_btn = Button(root, text="Dodaj ubranie", command=submit)
+submit_btn = Button(DataFrame, text="Dodaj ubranie", command=submit)
 submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 # Create View Butto
 
-view_btn = Button(text="Pokaż ubrnia", command=View)
+view_btn = Button(DataFrame, text="Pokaż ubrnia", command=View)
 view_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 # Commit Changes
